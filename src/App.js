96f2fcec593ab6127a793a1815021123db936b67
erfan ns <erfanns028@gmail.com>
2020-05-01 15:48:57 +0430

    Work with set state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      string: "You dont know me!"
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hey man.
+            { this.state.string }
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={ () => this.setState({string: "Tink you know me!"}) }>Click me!</button>
         </header>
       </div>
     );
