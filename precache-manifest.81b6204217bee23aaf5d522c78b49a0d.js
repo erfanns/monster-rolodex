c0ecbe0e9991ad7be873be5ccc52226d5e16127e
erfan ns <erfanns028@gmail.com>
2020-05-09 17:29:28 +0430

    Updates

---
+++ precache-manifest.81b6204217bee23aaf5d522c78b49a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ae5446eaf9c0469dd9ca64004e4f12",
+    "url": "/second-test/index.html"
+  },
+  {
+    "revision": "3d7dfccf2f9116c3f459",
+    "url": "/second-test/static/css/main.1a98dd6f.chunk.css"
+  },
+  {
+    "revision": "96915cbbcd72d479f22e",
+    "url": "/second-test/static/js/2.fdb0ed12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/second-test/static/js/2.fdb0ed12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7dfccf2f9116c3f459",
+    "url": "/second-test/static/js/main.0644d6f1.chunk.js"
+  },
+  {
+    "revision": "98e6c6d721e313a56551",
+    "url": "/second-test/static/js/runtime-main.eb8f3a01.js"
+  }
+]);
